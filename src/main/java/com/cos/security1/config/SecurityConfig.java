245df127419521207c773c459cdb3cc94f9305c4
lee-ji-an <leeja0407@naver.com>
2023-01-02 00:03:43 +0900

    Feat: Get Google profile information

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity  //스프링 시큐리티 필터가 스프링 필터체인에 등록됨.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)  // secured 어노테이션 활성화, preAuthorize & postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /* 카카오 로그인 api
+        1. 코드받기 (인증 성공, 정상적으로 api 에서 로그인이 됐다)
+        2. 액세스토큰 받기 (권한이 생김)
+        3. 사용자 프로필 가져옴
+        4-1. 그 정보를 토대로 회원가입을 자동으로 진행. (추가 정보 필요 없을 시)
+        4-2. 추가적인 구성정보를 요구하는 페이지로 (추가 정보 필요할 때)
+     */
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean  //리턴되는 오브젝트를 IoC로 등
     public BCryptPasswordEncoder encodePwd() {
@@ -35,7 +48,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")       // 로그인 완료된 후, '/' 를 반환함 (특정 페이지를 들어가려다 login 페이지로 들어왔을 경우, 해당 페이지로 갈 수 있게 해줌)
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");   // 구글 로그인이 완료된 뒤의 후처리가 필요
+                .loginPage("/loginForm")   // 구글 로그인이 완료된 뒤의 후처리가 필요  Tip. 구글 로그인 api 는? (oauth-client 만의 특징?) 코드 X, 액세스 토큰 + 사용자프로필정보 O
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);   // 후처리
     }
 
 }
